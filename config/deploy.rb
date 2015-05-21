b3fce53c7db840dc9122d1d08e1ed3fd0de8cdd0
JoelNietoTec <joelnieto_tec@yahoo.com>
2015-05-20 21:37:28 -0500

    Ajn

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.1.0'
 
 set :application, 'Sipanab'
-#set :repo_url, 'git@github.com/JoelNietoTec/Sipanab.git'
+set :repo_url, 'git@github.com/JoelNietoTec/Sipanab.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
