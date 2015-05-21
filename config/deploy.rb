387b93a5402c5a8e27a1202ad516bb762e7a3345
JoelNietoTec <joelnieto_tec@yahoo.com>
2015-05-20 21:35:03 -0500

    Deploy 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,13 @@
 lock '3.1.0'
 
 set :application, 'Sipanab'
-set :repo_url, 'git@github.com/JoelNietoTec/Sipanab.git'
+#set :repo_url, 'git@github.com/JoelNietoTec/Sipanab.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-set :deploy_to, '/home/deploy/Sipanab'
+#set :deploy_to, '/home/deploy/Sipanab'
 
 # Default value for :scm is :git
 # set :scm, :git
