85fa78adba3386b97faedf471d7812c0a920bed5
JoelNietoTec <joelnieto_tec@yahoo.com>
2015-05-21 23:45:56 -0500

    sdcdsc

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,9 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.assets.precompile += %w( .svg .eot .woff .ttf )
+  config.assets.precompile += %w[ *.svg *.eot *.woff *.ttf ]
+
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
