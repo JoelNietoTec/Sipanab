1952089f25cf1dfe266eb1478cbcc94eabbc638c
JoelNietoTec <joelnieto_tec@yahoo.com>
2015-06-08 23:14:50 -0500

    SummerNote v1.0

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,26 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
-//= require bootsy
+//= require summernote
+//= require summernote/locales/es-ES
+//= require summernote/plugin/summernote-ext-video.js
 //= require_tree ./shared
+
+$(document).ready(function() {
+  $('[data-provider="summernote"]').each(function(){
+    $(this).summernote({
+      lang: 'es-ES',
+      maximumImageFileSize: 1048576,
+      toolbar: [
+        ['style', ['style']],
+        ['style', ['bold', 'italic', 'underline']],
+        ['color', ['color']],
+        ['para', ['ul', 'ol', 'paragraph']],
+        ['height', ['height']],
+        ['table', ['table']],
+        ['insert', ['link', 'picture', 'video']],
+        ['misc', ['fullscreen', 'codeview', 'undo', 'redo', 'help']]
+      ]
+    });
+  })
+});
